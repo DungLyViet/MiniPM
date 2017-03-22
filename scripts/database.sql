7e352fc4ab007817e48afbb0f00f7ff4fde6ecff
Dung Ly Viet <dungharry92@gmail.com>
2017-03-22 12:39:20 +0700

    Initializing the project

---
+++ scripts/database.sql
@@ -0,0 +1,6 @@
+--	Author: Dung Ly Viet
+--	Date created: March 22th, 2017
+--
+--	Description: this is the sql script in the development period of MiniPM project
+
+create database if not exists MiniPM_development
---
+++ scripts/start_server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#	Author: Dung Ly Viet
+#	Date created: March 22th, 2017
+#
+#	Description: this is the bash scrip for starting rails server
+
+sudo rails server
